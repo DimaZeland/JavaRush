8d3bce82d1c7a44d9e5702f0e2c068eecffafc64
Dima <onenewinvest@gmail.com>
2022-07-26 18:50:36 +0300

    hard bonus tasks with synchronized done

---
+++ 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
@@ -0,0 +1,84 @@
+package com.javarush.task.task17.task1710;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+/* 
+CRUD
+*/
+
+public class Solution
+{
+    public static List<Person> allPeople = new ArrayList<Person>();
+
+    static
+    {
+        allPeople.add(Person.createMale("Иванов Иван", new Date()));  //сегодня родился    id=0
+        allPeople.add(Person.createMale("Петров Петр", new Date()));  //сегодня родился    id=1
+    }
+
+    public static void main(String[] args)
+    {
+        if (0 != args.length)
+        {
+            if ("-c".equals(args[0]))
+            {
+                SimpleDateFormat fm = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+                Date bd = null;
+                try
+                {
+                    bd = fm.parse(args[3]);
+                } catch (ParseException e)
+                {
+                    e.printStackTrace();
+                }
+
+                if ("м".equals(args[2]))
+                    allPeople.add(Person.createMale(args[1], bd));
+                else
+                    allPeople.add(Person.createFemale(args[1], bd));
+
+                System.out.println(allPeople.size() - 1);
+            } else if ("-r".equals(args[0]))
+            {
+                int id = Integer.parseInt(args[1]);
+
+                String name = allPeople.get(id).getName();
+                String sex = allPeople.get(id).getSex() == Sex.MALE ? "м" : "ж";
+                Date bd = allPeople.get(id).getBirthDate();
+                SimpleDateFormat fm = new SimpleDateFormat("dd-MMM-yyyy");
+                String bdStr = fm.format(bd).toString();
+
+                System.out.println(name + ' ' + sex + ' ' + bdStr);
+            } else if ("-u".equals(args[0]))
+            {
+                int id = Integer.parseInt(args[1]);
+                String name = args[2];
+                Sex sex = "м".equals(args[3]) ? Sex.MALE : Sex.FEMALE;
+                SimpleDateFormat fm = new SimpleDateFormat("dd/MM/yyyy");
+                Date bd = null;
+                try
+                {
+                    bd = fm.parse(args[4]);
+                } catch (ParseException e)
+                {
+                    e.printStackTrace();
+                }
+
+                allPeople.get(id).setName(name);
+                allPeople.get(id).setSex(sex);
+                allPeople.get(id).setBirthDate(bd);
+            } else if ("-d".equals(args[0]))
+            {
+                int id = Integer.parseInt(args[1]);
+                allPeople.get(id).setName(null);
+                allPeople.get(id).setSex(null);
+                allPeople.get(id).setBirthDate(null);
+            }
+        }
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
@@ -0,0 +1,48 @@
+package com.javarush.task.task17.task1721;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Транзакционность
+*/
+
+public class Solution {
+    public static List<String> allLines = new ArrayList<String>();
+    public static List<String> forRemoveLines = new ArrayList<String>();
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String fileName1 = "";
+        String fileName2 = "";
+
+        try
+        {
+            fileName1 = br.readLine();
+            fileName2 = br.readLine();
+
+        allLines = Files.readAllLines(Paths.get(fileName1));
+        forRemoveLines = Files.readAllLines(Paths.get(fileName2));
+
+        new Solution().joinData();
+        }
+        catch (IOException e)
+        {e.printStackTrace();}
+    }
+
+    public void joinData() throws CorruptedDataException {
+        if (allLines.containsAll(forRemoveLines))
+            allLines.removeAll(forRemoveLines);
+            else
+        {
+            allLines.clear();
+            throw new CorruptedDataException();
+        }
+    }
+}
