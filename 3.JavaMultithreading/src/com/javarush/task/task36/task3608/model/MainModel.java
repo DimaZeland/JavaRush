acff34b48682aff48a2d7acbd90d49724ebba0db
Dima <onenewinvest@gmail.com>
2022-08-20 12:44:10 +0300

    the MVC design pattern task is done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/MainModel.java
@@ -0,0 +1,63 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+import com.javarush.task.task36.task3608.model.service.UserService;
+import com.javarush.task.task36.task3608.model.service.UserServiceImpl;
+
+import java.util.List;
+
+public class MainModel implements Model
+{
+    private ModelData modelData = new ModelData();
+    private UserService userService = new UserServiceImpl();
+
+    @Override
+    public ModelData getModelData()
+    {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers()
+    {
+        modelData.setUsers(getAllUsers());
+        modelData.setDisplayDeletedUserList(false);
+    }
+
+    public void loadDeletedUsers() {
+        List<User> users = userService.getAllDeletedUsers();
+        modelData.setUsers(users);
+        modelData.setDisplayDeletedUserList(true);
+    }
+
+    public void loadUserById(long userId) {
+        User user = userService.getUsersById(userId);
+        modelData.setActiveUser(user);
+    }
+
+    public void deleteUserById(long id)
+    {
+        userService.deleteUser(id);
+       List<User> users = getAllUsers();
+       
+       modelData.setUsers(users);
+    }
+
+    private List<User> getAllUsers() {
+        //model should contain all business logic in the methods
+        List<User> allUsers = userService.getUsersBetweenLevels(1, 100);
+        allUsers = userService.filterOnlyActiveUsers(allUsers);
+        return allUsers;
+    }
+
+    public void changeUserData(String name, long id, int level)
+    {
+        userService.createOrUpdateUser(name,id,level);
+
+        List<User> users = getAllUsers();
+        //refresh model data
+        modelData.setUsers(users);
+    }
+
+
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/UsersView.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task36.task3608.view;
+
+import com.javarush.task.task36.task3608.bean.User;
+import com.javarush.task.task36.task3608.controller.Controller;
+import com.javarush.task.task36.task3608.model.ModelData;
+
+public class UsersView implements View
+{
+    private Controller controller;
+
+    @Override
+    public void refresh(ModelData modelData)
+    {
+
+        String str = modelData.isDisplayDeletedUserList() ? "All deleted users:" : "All users:";
+        System.out.println(str);
+
+        for (User user : modelData.getUsers())
+            System.out.println("\t"
+                    + String.format("User{name='%s', id=%d, level=%d}", user.getName(), user.getId(), user.getLevel()));
+
+        System.out.println("===================================================");
+    }
+
+    @Override
+    public void setController(Controller controller)
+    {
+        this.controller = controller;
+    }
+
+    public void fireEventShowAllUsers()
+    {
+        controller.onShowAllUsers();
+    }
+
+    public void fireEventShowDeletedUsers()
+    {
+        controller.onShowAllDeletedUsers();
+    }
+
+    public void fireEventOpenUserEditForm(long id)
+    {
+        controller.onOpenUserEditForm(id);
+    }
+}
+
