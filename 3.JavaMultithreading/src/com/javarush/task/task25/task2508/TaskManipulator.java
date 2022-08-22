e464d2d825631abf562bbb2fc0dc08c2449bbfad
Dima <onenewinvest@gmail.com>
2022-08-22 19:11:55 +0300

    thread interruption tasks are done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2508/TaskManipulator.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task25.task2508;
+
+public class TaskManipulator implements Runnable, CustomThreadManipulator
+{
+    private Thread field;
+
+    @Override
+    public void run()
+    {
+        
+        while (!Thread.currentThread().isInterrupted())
+        {
+            System.out.println(Thread.currentThread().getName());
+            try
+            {
+                Thread.sleep(100);
+            } catch (InterruptedException e)
+            {
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void start(String threadName)
+    {
+        field = new Thread(this, threadName);
+        field.start();
+    }
+
+    @Override
+    public void stop()
+    {
+        field.interrupt();
+    }
+}
