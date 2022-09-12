eba37eb68e9f2f7103849e1bfb25bce6192b8635
Dima <onenewinvest@gmail.com>
2022-09-12 15:41:58 +0300

    three tasks on calculus systems are done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3002/Solution.java
@@ -0,0 +1,68 @@
+package com.javarush.task.task30.task3002;
+
+/* 
+Осваиваем методы класса Integer
+*/
+
+public class Solution
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println(convertToDecimalSystem("0x16")); //22
+        System.out.println(convertToDecimalSystem("012"));  //10
+        System.out.println(convertToDecimalSystem("0b10")); //2
+        System.out.println(convertToDecimalSystem("62"));   //62
+    }
+
+    public static String convertToDecimalSystem(String s)
+    {
+        String digit = "";
+        int numberSystem = 0;
+
+        if (s.startsWith("0x"))
+        {
+            digit = s.substring(2, s.length());
+            numberSystem = 16;
+        }
+        else if (s.startsWith("0b"))
+        {
+            digit = s.substring(2, s.length());
+            numberSystem = 2;
+        }
+        else if (s.startsWith("0"))
+        {
+            digit = s.substring(1, s.length());
+            numberSystem = 8;
+        }
+        else
+        {
+            digit = s;
+            numberSystem = 10;
+        }
+
+        int decimalValue = Integer.parseInt(digit,numberSystem);
+
+        return String.valueOf(decimalValue);
+
+        //return convertToDecimal(digit, numberSystem);
+    }
+
+    public static String convertToDecimal(String digit, int numberSystem)
+    {
+        long decimalValue = 0;
+
+        // convert to decimal
+        for (int degree = digit.length() - 1, charIndex = 0; degree >= 0; degree--, charIndex++)
+        {
+            char ch = digit.charAt(charIndex);
+            int numeral = (int) ch > 96 ? (int) ch - 87 : Character.getNumericValue(ch); // 'a' has ANSI code 97
+
+            if (numeral >= numberSystem)
+                throw new NumberFormatException();
+
+            decimalValue += numeral * Math.pow(numberSystem, degree);
+        }
+        return String.valueOf(decimalValue);
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3009/Solution.java
@@ -0,0 +1,92 @@
+package com.javarush.task.task30.task3009;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Палиндром?
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        System.out.println(getRadix("112"));        //expected output: [3, 27, 13, 15]
+        System.out.println(getRadix("123"));        //expected output: [6]
+        System.out.println(getRadix("5321"));       //expected output: []
+        System.out.println(getRadix("1A"));         //expected output: []
+    }
+
+    private static Set<Integer> getRadix(String number) {
+        Set<Integer> result = new HashSet<>();
+        try {
+            int numberToProcess = Integer.parseInt(number);
+            for (int i = 2; i <= 36; i++) {
+                String temp = Integer.toString(numberToProcess, i);
+                if (isPalindrom(temp)) {
+                    result.add(i);
+                }
+            }
+        } catch (NumberFormatException e) {
+        }
+        return result;
+    }
+
+    private static boolean isPalindrom(String number) {
+        return number.equals((new StringBuilder(number)).reverse().toString());
+    }
+
+    /*private static Set<Integer> getRadix(String number)
+    {
+        HashSet<Integer> result = new HashSet<>();
+        String convertNumber = "";
+        try
+        {
+            for (int i = 2; i < 37; i++)
+            {
+                convertNumber = convertDecimalToOtherNumberSystem(number, i);
+                boolean isPalindrom = true;
+                for (int j = 0, k = convertNumber.length() - 1; k > j; j++, k--)
+                {
+                    if (convertNumber.charAt(j) != convertNumber.charAt(k))
+                    {
+                        isPalindrom = false;
+                        break;
+                    }
+                }
+                if (isPalindrom)
+                    result.add(i);
+            }
+        } catch (NumberFormatException e)
+        {
+        } finally
+        {
+            return result;
+        }
+    }
+
+    public static String convertDecimalToOtherNumberSystem(String number, int numberSystem) throws NumberFormatException
+    {
+        long decimalValue = 0;
+        decimalValue = Long.valueOf(number);
+        StringBuilder expectedValue = new StringBuilder();
+
+        while (true)
+        {
+            int numeral = (int) (decimalValue % numberSystem);
+
+            char ch = numberSystem > 10 ? (char) (numeral + 87) : Character.forDigit(numeral, 10);
+
+            expectedValue.insert(0, ch);
+            decimalValue /= numberSystem;
+
+            if (decimalValue < numberSystem)
+            {
+                ch = numberSystem > 10 ? (char) (decimalValue + 87) : Character.forDigit((int) decimalValue, 10);
+                expectedValue.insert(0, ch);
+                break;
+            }
+        }
+        return expectedValue.toString();
+    }*/
+}
