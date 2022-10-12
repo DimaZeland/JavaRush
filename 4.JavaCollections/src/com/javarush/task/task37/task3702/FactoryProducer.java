6d6ae1c0a03b68a9e7b3381a8046cad84454319e
Dima <onenewinvest@gmail.com>
2022-10-12 20:51:17 +0300

    Big task: Object creation pattern done

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/FactoryProducer.java
@@ -0,0 +1,20 @@
+
+package com.javarush.task.task37.task3702;
+
+import com.javarush.task.task37.task3702.female.FemaleFactory;
+import com.javarush.task.task37.task3702.male.MaleFactory;
+
+import static com.javarush.task.task37.task3702.FactoryProducer.HumanFactoryType.MALE;
+
+public class FactoryProducer
+{
+    public static enum HumanFactoryType
+    {
+        MALE, FEMALE
+    }
+
+    public static AbstractFactory getFactory(HumanFactoryType type)
+    {
+        return type == MALE ? new MaleFactory() : new FemaleFactory();
+    }
+}
