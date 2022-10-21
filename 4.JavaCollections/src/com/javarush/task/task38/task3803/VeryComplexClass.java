d0dec843316aeb2436b4c7b7eb6ac6a119e7bcbf
Dima <onenewinvest@gmail.com>
2022-10-21 15:16:03 +0300

    Level 8 | lecture 4 => tasks done

---
+++ 4.JavaCollections/src/com/javarush/task/task38/task3803/VeryComplexClass.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task38.task3803;
+
+/*
+Runtime исключения (unchecked exception)
+*/
+
+public class VeryComplexClass {
+    public void methodThrowsClassCastException() {
+        Object i = Integer.valueOf(42);
+        String s = (String)i;
+    }
+
+    public void methodThrowsNullPointerException() {
+        Integer n1 = null;
+        System.out.println(n1.toString());
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
