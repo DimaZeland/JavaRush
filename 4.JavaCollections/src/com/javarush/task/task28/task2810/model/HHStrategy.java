a6c545b6a95d054f9ceca852e4d37b7850f5e651
Dima <onenewinvest@gmail.com>
2022-10-26 07:57:39 +0300

    Level 8 | Aggregator in Java tasks 1-7 are done

---
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task28.task2810.model;
+
+import com.javarush.task.task28.task2810.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HHStrategy implements Strategy {
+
+    private static final String URL_FORMAT = "https://hh.ru/search/vacancy?text=java+%s&page=%d";
+
+    @Override
+    public List<Vacancy> getVacancies(String searchString) {
+        try {
+            Document document = Jsoup.connect(String.format(URL_FORMAT, "MyTown", 0))
+                    .userAgent("Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Mobile Safari/537.36")
+                    .referrer("https://www.google.com.ua/")
+                    .get();
+            System.out.println(12345);;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+}
