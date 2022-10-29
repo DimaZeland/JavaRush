964e366e952fae7c78700c8bbb5887b29aee435d
Dima <onenewinvest@gmail.com>
2022-10-29 14:35:30 +0300

    Level 39 | lecture 4 => tasks are done

---
+++ 4.JavaCollections/src/com/javarush/task/task40/task4011/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task40.task4011;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/* 
+Свойства URL
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        decodeURLString("https://www.amrood.com/index.htm?language=en#j2se");
+    }
+
+    public static void decodeURLString(String s) throws MalformedURLException {
+        try
+        {
+            URL url = new URL(s);
+            System.out.println("URL is " + url.toString());
+            System.out.println("Protocol: " + url.getProtocol());
+            System.out.println("Authority: " + url.getAuthority());
+            System.out.println("File: " + url.getFile());
+            System.out.println("Host: " + url.getHost());
+            System.out.println("Path: " + url.getPath());
+            System.out.println("Port: " + url.getPort());
+            System.out.println("DefaultPort: " + url.getDefaultPort());
+            System.out.println("Query: " + url.getQuery());
+            System.out.println("Ref: " + url.getRef());
+        } catch (Exception e)
+        {
+            System.out.println("Parameter " + s + " is not a valid URL.");
+        }
+    }
+}
+
