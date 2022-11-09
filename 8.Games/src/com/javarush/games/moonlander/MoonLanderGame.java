387042a24357a4b204825f9f86c7025591c4a66d
Dima <onenewinvest@gmail.com>
2022-11-09 16:25:34 +0200

    Game Moon Lander => task 5 is done

--- 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
+++ 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
@@ -2,30 +2,29 @@ package com.javarush.games.moonlander;
 
 import com.javarush.engine.cell.*;
 
-public class MoonLanderGame extends Game
-{
+public class MoonLanderGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
+    private Rocket rocket;
+
     @Override
-    public void initialize()
-    {
+    public void initialize() {
         setScreenSize(WIDTH, HEIGHT);
         createGame();
     }
 
-    private void createGame()
-    {
+    private void createGame() {
+        rocket = new Rocket(WIDTH / 2.0, 0);
         drawScene();
     }
 
-    private void drawScene()
-    {
-        for (int i = 0; i < WIDTH; i++)
-        {
-            for (int j = 0; j <HEIGHT ; j++)
-            {
-                setCellColor(i, j, Color.ORANGE);
+    private void drawScene() {
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                setCellColor(x, y, Color.BLACK);
             }
         }
+
+        rocket.draw(this);
     }
 }
--- 8.Games/src/com/javarush/games/moonlander/Rocket.java
+++ 8.Games/src/com/javarush/games/moonlander/Rocket.java
@@ -1,10 +1,7 @@
 package com.javarush.games.moonlander;
 
-public class Rocket extends GameObject
-{
-
-    public Rocket(double x, double y)
-    {
+public class Rocket extends GameObject {
+    public Rocket(double x, double y) {
         super(x, y, ShapeMatrix.ROCKET);
     }
 }
